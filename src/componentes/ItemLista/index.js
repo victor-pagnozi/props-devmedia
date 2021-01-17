46a8641b4009b0fa2f50da8780379c0421edea30
victor-pagnozi <victor.pagnozi@gmail.com>
2021-01-17 16:51:04 -0300

    Finalizado aula -- exibindo já lista com todos os signos por props e components.

--- src/componentes/ItemLista/index.js
+++ src/componentes/ItemLista/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './estilo.css';
 
-export default function ItemLista() {
+export default function ItemLista(props) {
   return (
     <div className="boxSigno">
       <div className="nomeSigno">
-        Aquário
+        { props.signo }
       </div>
       
-      <img className="imagemSigno" src="/assets/aquario.jpg" alt="Aquário"/>
+      <img className="imagemSigno" src={ props.imagem } alt={ props.signo }/>
       
       <div className="periodoNascimento">
-        21/01 - 19/02
+        { props.dataInicio } - { props.dataFim }
       </div>
     </div>
   )
